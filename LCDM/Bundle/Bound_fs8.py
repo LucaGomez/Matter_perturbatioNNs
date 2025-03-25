c42097b7e16431eef5f980a3fba695cfaba426fc
LucaGomez <90295935+LucaGomez@users.noreply.github.com>
2025-03-25 14:00:11 -0300

    Create Bound_fs8.py

---
+++ LCDM/Bundle/Bound_fs8.py
@@ -0,0 +1,378 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 21 16:08:50 2024
+
+@author: lgomez
+"""
+
+
+import torch
+import numpy as np
+from neurodiffeq import diff  # the differentiation operation
+from scipy.integrate import solve_ivp
+from matplotlib import pyplot as plt
+from bound_XY import EtaEstimation
+from neurodiffeq.solvers import BundleSolution1D
+from neurodiffeq.conditions import BundleIVP
+
+params = {
+    'axes.labelsize': 13,
+    'font.size': 11,
+    'legend.fontsize': 11,
+    'xtick.labelsize': 11,
+    'ytick.labelsize': 11,
+    'text.usetex': False,
+    'mathtext.fontset': 'cm',
+    'mathtext.rm': 'serif',
+    'font.sans-serif': 'Times',
+    "mathtext.fontset": 'cm',
+    'font.family': 'serif',
+    }
+plt.rcParams.update(params)
+
+'''Set up the parameters of the problem
+'''
+t_min = -1                  # The min value of the indep. variable
+t_max = 0                   # The max value of the indep. variable
+Om_r_0 = 5.38e-5            # The value of \Omega_radiation today
+sigma_8 = 0.8               # The value of the cosmological parameter \sigma_8
+a_i = 1e-3                  # The initial value of the scale factor
+n_i = np.abs(np.log(a_i))   # A relevant parameter in the equations
+X_0 = -n_i                  # Initial condition for X(N)
+Y_0 = n_i                   # Initial condition for Y(N)
+Om_m_0_min = 0.15           # The min value of \Omega_matter today
+Om_m_0_max = 0.4            # The max value of \Omega_matter today
+K = 50                      # The number of \Omegas during the exploration
+Om_m_0s = np.linspace(Om_m_0_min, Om_m_0_max, K)
+
+'''Define the relevant functions
+'''
+def G_eff_over_G(a): # The ratio between G_eff/G. Is trivial in LCDM.
+    return 1
+
+def C(t): # The value of the function C. Is trivial in matter perturbation.
+    return 1
+
+def B(t,Om_m_0): # The value of the function B.
+    Om_Lambda_0 = 1 - Om_r_0 - Om_m_0
+    a_eq = Om_r_0/Om_m_0
+    alpha = (a_eq**4)*(Om_Lambda_0/Om_r_0)
+    N_hat = t
+    if torch.is_tensor(t):
+        a = torch.exp(N_hat*n_i)
+    else:
+        a = np.exp(N_hat*n_i)
+    N = n_i*(1 + 4*alpha*((a/a_eq)**3))
+    D = 2*(1 + (a_eq/a) + alpha*((a/a_eq)**3))
+    out = N/D
+    return out
+
+def A(t,Om_m_0): # The value of the function A.
+    Om_Lambda_0 = 1 - Om_r_0 - Om_m_0
+    a_eq = Om_r_0/Om_m_0
+    alpha = (a_eq**4)*(Om_Lambda_0/Om_r_0)
+    N_hat = t
+    if torch.is_tensor(t):
+        a = torch.exp(N_hat*n_i)
+    else:
+        a = np.exp(N_hat*n_i)
+    N = -3*a*(n_i**2)*G_eff_over_G(a)
+    D = 2*a_eq*(1 + (a/a_eq) + alpha*((a/a_eq)**4))
+    out = N/D
+    return out
+
+def u(t,Om_m_0): # The numerical solver. This definition can be improved.
+    def matter_pert_eqs_for_num(t, variables):
+        Y = variables[1]
+        dX = Y
+        dY = -C(t)*Y**2 - B(t,Om_m_0)*Y - A(t,Om_m_0)
+        return [dX, dY]
+    cond_Num = [X_0, Y_0]
+    sol = solve_ivp(matter_pert_eqs_for_num, (t_min, t_max), cond_Num,
+                    method='RK45', rtol=1e-11, atol=1e-16, t_eval=t,)
+    return sol.y
+
+def res(X, Y, t, Om_m_0): # The residuals of the NNs.
+    res1 = diff(X, t) - Y
+    res2 = diff(Y, t) + C(t)*Y**2 + B(t, Om_m_0)*Y + A(t, Om_m_0)
+    return [res1, res2]
+
+def make_bound_delta(X,B_X): # The bound in \delta.
+    return np.exp(X)*B_X
+
+
+def make_bound_g(X,Y,B_X,B_Y): # The bound in \delta.
+    return np.sqrt((X*B_Y)**2+(Y*B_X)**2)
+
+def make_bound_g_TVM(X,Y,B_X,B_Y):
+    bound_grad = []
+    
+    lbx = X-B_X
+    ubx = X+B_X
+    lby = Y-B_Y
+    uby = Y+B_Y
+        
+    for i in range(len(X)):
+        max_grad = 0
+        for j in range(len(X)):
+            if lbx[i] <= X[j] <= ubx[i] and lby[i] <= Y[j] <= uby[i]:
+                
+                grad = np.exp(X[j]+Y[j])
+                if grad > max_grad:
+                    max_grad = grad
+                    
+        bound_grad.append(max_grad)
+        #print('ok')
+    bound_grad = np.array(bound_grad)
+    
+    return np.sqrt(2)*bound_grad*np.sqrt(B_X**2+B_Y**2)
+
+def make_bound_fs8_TVM(X,Y,B_X,B_Y,sigma_8):
+    bound_grad = []
+    U = X[-1]
+    B_U = B_X[-1]
+    lbx = X-B_X
+    ubx = X+B_X
+    lby = Y-B_Y
+    uby = Y+B_Y
+    lbu = U-B_U
+    ubu = U+B_U
+    
+        
+    for i in range(len(X)):
+        max_grad = 0
+        for j in range(len(X)):
+            if lbx[i] <= X[j] <= ubx[i] and lby[i] <= Y[j] <= uby[i]:
+                
+                grad = np.exp(X[j]-lbu)*np.sqrt(2*Y[j]**2+1)
+                if grad > max_grad:
+                    max_grad = grad
+                    
+        bound_grad.append(max_grad)
+        #print('ok')
+    bound_grad = np.array(bound_grad)
+    
+    return sigma_8*bound_grad*np.sqrt(B_X**2+B_Y**2+B_U**2)/n_i
+
+        
+def make_bound_delta_prime(N,X,Y,B_X,B_Y): # The bound in \delta\prime.
+    a = np.exp(n_i*N)
+    return (np.exp(X)/(n_i*a))*np.sqrt(B_X**2*Y**2+B_Y**2)
+
+'''
+def make_bound_fs8(N,X,Y,B_X,B_Y,sigma_8): # The bound in f\sigma_8.
+    a = np.exp(n_i*N)
+    bound_d = make_bound_delta(X,B_X)
+    bound_d_0 = bound_d[-1]
+    bound_dp = make_bound_delta_prime(ts,X,Y,B_X,B_Y)
+    delta = np.exp(X)
+    delta_0 = delta[-1]
+    delta_p = (np.exp(X)*Y)/(n_i*a)
+    return (sigma_8*a/(delta_0))*np.sqrt(bound_dp**2+(bound_d_0*delta_p/delta_0)**2)
+'''
+def make_bound_fs8(N,X,Y,B_X,B_Y,sigma_8): # The bound in f\sigma_8.
+    a = np.exp(n_i*N)
+    bound_d = make_bound_delta(X,B_X)
+    bound_d_0 = bound_d[-1]
+    bound_dp = make_bound_delta_prime(ts,X,Y,B_X,B_Y)
+    delta = np.exp(X)
+    delta_0 = delta[-1]
+    delta_p = (np.exp(X)*Y)/(n_i*a)
+    return bound_dp
+
+
+def make_bound_fs8_U(N,X,Y,B_X,B_Y,sigma_8): # The bound in f\sigma_8.
+    B_U = B_X[-1]
+    fact = np.exp(X-X[-1]*np.ones_like(X))
+    return (sigma_8*fact/n_i)*np.sqrt(Y**2*(B_X**2+B_U**2)+B_Y**2)
+
+'''Load the trained networks.
+'''
+nets = torch.load('nets_LCDM_bundle.ph', map_location='cpu')
+
+'''Set the initial conditions imposed during the training.
+'''
+conditions = [BundleIVP(t_min, X_0), BundleIVP(t_min, Y_0)]
+
+'''Call the solution and set relevant parameters to compute the bounds.
+'''
+v_sol = BundleSolution1D(nets, conditions)
+j_max = 3
+N_for_int = int(1e4)
+ts = np.linspace(t_min, t_max, N_for_int)
+a = np.exp(n_i*ts)
+
+'''Define an empty list to fill with the bounds computed.
+'''
+b_x = []
+b_y = []
+
+diff_x = []
+diff_y = []
+
+b_d = []
+b_dp = []
+
+diff_d = []
+diff_dp = []
+
+b_fs8 = []
+diff_fs8 = []
+
+'''Now iterate for the selected values of \Omega_matter today.
+'''
+for i in range(len(Om_m_0s)):
+    print(i)
+    '''Call the class to estimate the bounds for each value of omega matter.
+    '''
+    eta_module = EtaEstimation(v_sol=v_sol, Om_m_0=Om_m_0s[i], v_index=1, res_fun=res, B_fun=B, C_fun=C, t_min=t_min, t_max=t_max, N_for_int=N_for_int, j_max=j_max)
+    eta_module.compute_etas()
+    
+    bound_y = eta_module.make_eta_bound(J=j_max)(ts)
+    bound_x = eta_module.make_eta_bound_X(j_max=j_max)(ts)
+    
+    '''Call the solution of the neural networks for each value of omega matter and compute
+       value of delta prime.
+    '''
+    X_hats, Y_hats = v_sol(ts, Om_m_0s[i]*np.ones_like(ts), to_numpy=True)
+    delta = np.exp(X_hats)
+    delta_p = np.exp(X_hats)*Y_hats/(n_i*a)
+    fs8_nn = sigma_8*a*delta_p/np.exp(X_hats[-1])
+
+    X_num, Y_num = u(ts, Om_m_0s[i])
+    delta_num = np.exp(X_num)
+    delta_p_num = np.exp(X_num)*Y_num/(n_i*a)
+    fs8_num = sigma_8*a*delta_p_num/np.exp(X_num[-1])
+    
+    '''Compute the bounds in X and Y explicitly using the class called before.
+    '''
+    
+    bound_d = make_bound_delta(X_hats,bound_x)
+    bound_dp = make_bound_delta_prime(ts,X_hats,Y_hats,bound_x,bound_y)
+    
+    #bound_fs8 = make_bound_fs8(ts,X_hats,Y_hats,bound_x,bound_y,sigma_8)
+    #bound_fs8_U = make_bound_fs8_U(ts,X_hats,Y_hats,bound_x,bound_y,sigma_8)
+    bound_fs8_TVM = make_bound_fs8_TVM(X_hats,Y_hats,bound_x,bound_y,sigma_8)
+    
+    #bound_g = make_bound_g_TVM(X_hats, Y_hats, bound_x, bound_y)
+    #g_nn = np.exp(X_hats+Y_hats)
+    #g_num = np.exp(Y_num+X_num)
+    
+    b_fs8.append(100*(bound_fs8_TVM)/fs8_nn)
+    diff_fs8.append((bound_fs8_TVM-np.abs(fs8_nn-fs8_num))/fs8_nn)
+    '''
+    plt.figure()
+    plt.plot(ts, 100*(bound_fs8_TVM)/fs8_nn, label = 'Bound')
+    #plt.plot(ts, (bound_fs8_U), label = 'Bound U')
+    plt.plot(ts, 100*np.abs(fs8_nn-fs8_num)/fs8_nn, label = 'Num diff')
+    plt.xlabel('N')
+    plt.ylabel('err%')
+    plt.title(str(Om_m_0s[i]))
+    plt.legend()
+    plt.savefig(str(i)+'.png')
+    plt.show()
+    '''
+#%%
+b_fs8 = np.array(b_fs8)
+diff_fs8 = np.array(diff_fs8)
+
+plt.figure()
+plt.pcolormesh(np.exp(n_i*ts), Om_m_0s, b_fs8, cmap='viridis', rasterized = True)  # 'viridis' es un mapa de colores, puedes elegir otro
+plt.colorbar(label=r'$100*B_{f\sigma_8}/f\sigma_8^{NN}$')
+plt.xlabel(r'$a$')
+plt.ylabel(r'$\Omega_{m0}$')
+plt.xscale('log')
+plt.savefig('heatmap_fs8_LCDM.pdf')
+plt.show()
+
+plt.figure()
+plt.pcolormesh(np.exp(n_i*ts), Om_m_0s, diff_fs8, cmap='viridis', rasterized = True)  # 'viridis' es un mapa de colores, puedes elegir otro
+plt.colorbar(label=r'($B_{f\sigma_8} - |f\sigma_8^{NN}-f\sigma_8^{Num}|) /f\sigma_8^{NN}$')
+plt.xlabel(r'$a$')
+plt.ylabel(r'$\Omega_{m0}$')
+plt.xscale('log')
+plt.savefig('heatmap_fs8_LCDM_num_diff.pdf')
+plt.show()
+
+#%%
+res_fs8 = np.array(b_fs8) - np.array(diff_fs8)
+
+plt.figure()
+plt.pcolormesh(np.exp(n_i*ts), Om_m_0s, res_fs8, cmap='viridis', rasterized = True)  # 'viridis' es un mapa de colores, puedes elegir otro
+plt.colorbar(label='bound - numerical diff.')
+plt.xlabel(r'$a$')
+plt.ylabel(r'$\Omega_{m0}$')
+plt.savefig('res_fs8_LCDM.pdf')
+plt.title(str(np.min(res_fs8)))
+plt.show()
+#%%
+res_x = np.array(b_x) - np.array(diff_x)
+res_y = np.array(b_y) - np.array(diff_y)    
+
+res_d = np.array(b_d) - np.array(diff_d)
+res_dp = np.array(b_dp) - np.array(diff_dp) 
+
+plt.figure()
+plt.pcolormesh(ts, Om_m_0s, res_x, cmap='viridis')  # 'viridis' es un mapa de colores, puedes elegir otro
+plt.colorbar()
+plt.xlabel(r'$a$')
+plt.ylabel(r'$\Omega_{m0}$')
+plt.title(str(np.min(res_x)))
+#plt.savefig('heatmap_fs8.png')
+plt.show()
+
+plt.figure()
+plt.pcolormesh(ts, Om_m_0s, res_y, cmap='viridis')  # 'viridis' es un mapa de colores, puedes elegir otro
+plt.colorbar()
+plt.xlabel(r'$a$')
+plt.ylabel(r'$\Omega_{m0}$')
+plt.title(str(np.min(res_y)))
+#plt.savefig('heatmap_fs8.png')
+plt.show()
+
+
+#%%
+'''Now define the data values to plot the bound in a relevant range.
+'''
+z_dat = [0.17, 0.02, 0.02, 0.44, 0.60, 0.73, 0.18, 0.38, 1.4, 0.02, 0.6, 0.86, 0.03, 0.013, 0.15, 0.38, 0.51, 0.70, 0.85, 1.48]
+z_dat = np.array(z_dat)
+a_dat = 1/(1+z_dat)
+N_hat_dat = np.log(a_dat)
+N_dat = N_hat_dat/n_i
+'''Plot the bounds in the range defined before.
+'''
+plt.figure()
+plt.pcolormesh(np.exp(n_i*ts), Om_m_0s, b_x, cmap='viridis')  # 'viridis' es un mapa de colores, puedes elegir otro
+plt.colorbar()
+plt.xlabel(r'$a$')
+plt.xlim(np.min(a_dat),np.max(a_dat))
+plt.ylabel(r'$\Omega_{m0}$')
+plt.title(r'%bound $f\sigma_8$')
+#plt.savefig('heatmap_fs8.png')
+plt.show()
+
+plt.figure()
+plt.pcolormesh(np.exp(n_i*ts), Om_m_0s, diff_x, cmap='viridis')  # 'viridis' es un mapa de colores, puedes elegir otro
+plt.colorbar()
+plt.xlabel(r'$a$')
+plt.xlim(np.min(a_dat),np.max(a_dat))
+plt.ylabel(r'$\Omega_{m0}$')
+plt.title(r'%err $f\sigma_8$ (numerical)')
+#plt.savefig('heatmap_fs8.png')
+plt.show()
+
+#%%
+residual = np.array(b_fs8_per) - np.array(diff_fs8)
+
+plt.figure()
+plt.pcolormesh(np.exp(n_i*ts), Om_m_0s, residual, cmap='viridis')  # 'viridis' es un mapa de colores, puedes elegir otro
+plt.colorbar()
+plt.xlabel(r'$a$')
+plt.xlim(np.min(a_dat),np.max(a_dat))
+plt.ylabel(r'$\Omega_{m0}$')
+plt.title(r'bound% - err%')
+#plt.savefig('heatmap_fs8.png')
+plt.show()
+
+print(np.min(residual))
